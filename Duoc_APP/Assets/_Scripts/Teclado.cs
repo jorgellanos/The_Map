4a1318b8ebebd0e07ba7549005759ff618762947
JorgeLlanos <jorg.llanos@hotmail.com>
2019-08-31 21:30:36 -0400

    Rescue STUFF
    iconos y texturas y scripts del proyecto anteriori

---
+++ Duoc_APP/Assets/_Scripts/Teclado.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Teclado : MonoBehaviour {
+
+    //variables
+    string word = string.Empty;
+    public InputField input = null;
+
+    void Update()
+    {
+        if (input.text == "")
+        {
+            word = "";
+        }
+    }
+
+    public void funcion (string letra)
+    {
+        //agregar letra al inputfield
+        word = word + letra;
+        input.text = word.ToUpper();
+	}
+
+    public void backSpace()
+    {
+        if(input.text.Length > 0)
+        {
+            //eliminar/remover ultimo caracter del inputfield
+            input.text = input.text.Remove(input.text.Length - 1, 1);
+            word = input.text;
+        }
+    }
+
+}
