14ee5bdebb4d231bac4b3c8136e844322ce1664d
Llanos <jorg.llanos@hotmail.com>
2019-11-05 16:27:54 -0300

    background image

--- Duoc_APP/Assets/_Scripts/Manager.cs
+++ Duoc_APP/Assets/_Scripts/Manager.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
     public GameObject[] Canvases;
-    public string filePath;
+    public string filePath, backgroundPath;
+    public bool loading;
 
     // Start is called before the first frame update
     void Start()
     {
+        loading = true;
         ReadJson();
         Canvases[0].gameObject.SetActive(true);
         Canvases[1].gameObject.SetActive(false);
@@ -21,7 +24,26 @@ public class Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (loading)
+        {
+            LoadBackground();
+        }
+    }
+
+    public void LoadBackground()
+    {
+        for (int i = 0; i < Canvases.Length; i++)
+        {
+            if (i >= Canvases.Length)
+            {
+                loading = false;
+            }
+            else
+            {
+                Canvases[i].gameObject.transform.Find("Fondos").GetComponent<Image>().sprite
+                    = ImportImageToSprite(backgroundPath + i + ".jpg");
+            }
+        }
     }
 
     public void ReadJson()
@@ -32,6 +54,29 @@ public class Manager : MonoBehaviour
             Persona p = JsonUtility.FromJson<Persona>(json);
             Debug.Log(json);
         }
+    }
+
+    public Sprite ImportImageToSprite(string filePath)
+    {
+        Texture2D tex = null;
+        Rect rec;
+        byte[] fileData;
+
+        if (File.Exists(filePath))
+        {
+            fileData = File.ReadAllBytes(filePath);
+            tex = new Texture2D(2, 2);
+            tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+            Debug.Log("File Exists!");
+            rec = new Rect(0, 0, tex.width, tex.height);
+        }
+        else
+        {
+            Debug.LogError("File not exists!");
+            rec = new Rect(0, 0, 0, 0);
+        }
+
+        return Sprite.Create(tex, rec, new Vector2(0.5f, 0.5f), 100);
     }
 }
 
